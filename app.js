15ccac03835a71da030f5f2a981c1710c41508ff
AmraNK9 <107979459+AmraNK9@users.noreply.github.com>
2022-08-30 16:08:56 +0630

    No error Code

---
+++ app.js
@@ -0,0 +1,69 @@
+const answer = Array.from(document.querySelectorAll(".answer"))
+const questionDb = document.querySelector("#question");
+
+const Mainquestion = [
+    {
+        question:"what is the answer of these question",
+        choice1:"choice1",
+        choice2:"choice2",
+        choice3:"choice3",
+        choice4:"choice4",
+        answer:"1"
+    },
+    {
+        question:"What is the famous japan actor ",
+        choice1:"thu ta",
+        choice2:"yo ma ti",
+        choice3:"kim mot shi",
+        choice4:"Nay Htoo Naing",
+        answer:"3",
+    },
+    {
+        question:"What is the 2 japan actor ",
+        choice1:"thu ta",
+        choice2:"yo ma ti",
+        choice3:"kim mot shi",
+        choice4:"Nay Htoo Naing",
+        answer:"2",
+    },
+]
+
+//functionSetup
+const question = [...Mainquestion];
+
+const showQuestion = ()=>{
+    if(question.length === 0){
+        return window.location.assign("/end.html")
+        
+    }
+    const  questionId = Math.floor(Math.random()*question.length);
+questionDb.innerHTML = question[questionId].question;
+
+
+answer.forEach(
+    (item)=>{
+        let forChoice = "choice"+item.dataset["number"];
+      
+        item.innerHTML = `${question[questionId][forChoice]}`
+    }
+)
+    
+for(let i=0;i<answer.length;i++){
+answer[i].addEventListener("click",
+(e)=>{
+    console.log("this is work"+i);
+  
+    console.log(e.target.dataset["number"]);
+    if(question[questionId].answer==e.target.dataset["number"]){
+       console.log("this is true");
+       question.splice(questionId,1);
+       showQuestion();
+    }
+}
+)
+}
+
+
+}
+
+showQuestion();
